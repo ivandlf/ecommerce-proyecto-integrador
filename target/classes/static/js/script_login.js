24eec881a247be527a8396738e5c533ed340a975
Ivan Dlf <98287091+ivandlf@users.noreply.github.com>
2023-10-30 13:21:12 -0300

    implementando authentication

--- target/classes/static/js/script_login.js
+++ target/classes/static/js/script_login.js
@@ -3,18 +3,48 @@ const $btnSignIn= document.querySelector('.sign-in-btn'),
       $signUp = document.querySelector('.sign-up'),
       $signIn  = document.querySelector('.sign-in');
 const btnIniciarSesion = document.querySelector('#btnIniciarSesion')
+const btnRegistrarse = document.querySelector('#btnRegistrarse')
 
 document.addEventListener('click', e => {
     if (e.target === $btnSignIn || e.target === $btnSignUp) {
         $signIn.classList.toggle('active');
         $signUp.classList.toggle('active')
+        
     }
     if (e.target == $btnSignIn){
-        iniciarSesion();
+      console.log('boton sign in')
+    }
+    if (e.target == $btnSignUp){
+      console.log('boton sign up')
+      
     }
 });
 
 btnIniciarSesion.addEventListener("click", iniciarSesion)
+
+btnRegistrarse.addEventListener("click", crearUsuario)
+async function crearUsuario(){
+  let datos = {};
+  datos.nombre = document.getElementById('txtNombreSingUp').value;
+  datos.email = document.getElementById('txtemailSingUp').value;
+  datos.password = document.getElementById('txtpassSingUp').value;
+  console.log(datos)
+  fetch('api/user', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  })
+  .then(response => {
+    if(response.ok){
+      console.log("usuario creado con exito")
+    }else{
+      console.log("fallo al crear usuario")
+    }
+  })
+}
 
 async function iniciarSesion() {
     let datos = {};
