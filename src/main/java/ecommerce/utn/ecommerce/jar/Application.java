0374fa4c7ee802a12327837572df4cafb2004c41
Ivan Dlf <98287091+ivandlf@users.noreply.github.com>
2023-11-02 20:56:42 -0300

    actualizando front

--- src/main/java/ecommerce/utn/ecommerce/jar/Application.java
+++ src/main/java/ecommerce/utn/ecommerce/jar/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		MercadoPagoConfig.setAccessToken("PROD_ACCESS_TOKEN");
 		SpringApplication.run(Application.class, args);
 	}
 
